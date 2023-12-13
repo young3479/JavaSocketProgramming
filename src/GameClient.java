e5cd4beb279f60ec5e5b9878c44dfcd076f1edec
yoonseopkim <qc70045@gmail.com>
2023-12-13 23:14:24 +0900

    1213 맵에 플레이어 2명 띄움

--- src/GameClient.java
+++ src/GameClient.java
@@ -1,5 +1,6 @@
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import java.awt.*;
 
 public class GameClient {
     public static void main(String[] args) {
@@ -14,22 +15,24 @@ public class GameClient {
         JFrame frame = new JFrame("Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        GamePanel gamePanel = new GamePanel(); // GamePanel 인스턴스 생성
+
 
         // 플레이어 생성
-        Player player1 = new Player(1, 200, 200); // 예시 좌표
-        Player player2 = new Player(2, 205, 300); // 예시 좌표
+        Player player1 = new Player(1, 200, 200, Color.red);
+        Player player2 = new Player(2, 205, 300, Color.yellow);
 
+
+        GamePanel gamePanel = new GamePanel(player1, player2); // 수정된 생성자
         // 플레이어를 게임 패널에 추가
-        gamePanel.add(player1);
-        gamePanel.add(player2);
+//        gamePanel.add(player1);
+//        gamePanel.add(player2);
 
 
         // 서버 연결 설정 (IP 주소와 포트 번호에 따라)
         gamePanel.connectToServer("127.0.0.1", 30000);
 
         frame.add(gamePanel);
-        frame.setSize(1600, 900); // 적절한 크기로 설정
+        frame.setSize(800, 600); // 적절한 크기로 설정
         frame.setVisible(true);
     }
 }
