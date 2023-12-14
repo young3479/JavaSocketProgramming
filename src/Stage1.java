6596c4a162369c3aa6c877389067c23352201153
chaeyoung <chae00901@naver.com>
2023-12-14 10:14:51 +0900

    block add

--- src/Stage1.java
+++ src/Stage1.java
@@ -19,19 +19,19 @@ public class Stage1 {
         // 블럭추가
 
         // 상단
-        platforms.add(new Platform(0, 0, panelWidth, borderThickness,"Image/block/block.png"));
+        platforms.add(new Platform(0, 0, panelWidth, borderThickness,"Image/block/block2.png"));
         // 하단
-        platforms.add(new Platform(0, panelHeight - borderThickness, panelWidth, borderThickness, "Image/block/block.png"));
+        platforms.add(new Platform(0, panelHeight - borderThickness, panelWidth, borderThickness, "Image/block/block2.png"));
         // 왼쪽
-        platforms.add(new Platform(0, 0, borderThickness, panelHeight, "Image/block/block.png"));
+        platforms.add(new Platform(0, 0, borderThickness, panelHeight, "Image/block/block3.png"));
         // 오른쪽
-        platforms.add(new Platform(panelWidth - borderThickness, 0, borderThickness, panelHeight, "Image/block/block.png"));
+        platforms.add(new Platform(panelWidth - borderThickness, 0, borderThickness, panelHeight, "Image/block/block3.png"));
 
         // 계단
-        platforms.add(new Platform(100, 500, 100, 20, "Image/block/block.png"));
-        platforms.add(new Platform(250, 400, 100, 20, "Image/block/block.png"));
-        platforms.add(new Platform(400, 300, 100, 20, "Image/block/block.png"));
-        platforms.add(new Platform(550, 200, 100, 20, "Image/block/block.png"));
+        platforms.add(new Platform(100, 500, 550, 20, "Image/block/block2.png"));
+        platforms.add(new Platform(250, 400, 100, 20, "Image/block/block2.png"));
+        platforms.add(new Platform(400, 300, 100, 20, "Image/block/block2.png"));
+        platforms.add(new Platform(550, 200, 100, 20, "Image/block/block2.png"));
     }
 
 
